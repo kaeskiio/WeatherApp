9c80406340cca4cedcf5a24d14548fe2da55f3cb
kacemettahali <151705697+kacemettahali@users.noreply.github.com>
2024-01-16 19:26:29 -0600

    Add files via upload

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Kacem Ettahali on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
